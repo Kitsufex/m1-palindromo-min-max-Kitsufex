64f02db657bf00416d1911490f0845b2e9afbae9
Stephanne Lima <automata.s012@gmail.com>
2024-04-13 11:11:24 -0300

    Teste do Palíndromo e Min Max

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 function isPalindrome(str){
     /* Seu código aqui */
+    str = str.replaceAll("", "").toLowerCase();
+    let new_str = "";
+
+    for (let i = str.length; i > 0; i--) {
+        new_str +=str.charAt(i - 1);
+    }
+
+    return new_str == str;
 }
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 0; i < arr.lenght; i++) {
+        if (arr[i] < min) {
+          min = arr[i];
+        } else if (arr[i] > max) {
+          max = arr[i];
+        }
+    }
+
+    return [min, max];
 }
